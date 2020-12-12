df3a523e4a5b77774621888680a793cf4d71be5e
Aakash Belide <aakash.belide@gmail.com>
2020-12-13 00:40:31 +0530

    For app

--- chatbot/app.py
+++ chatbot/app.py
@@ -69,5 +69,54 @@ def sms_reply():
     
         return str(resp)
 
+@app.route("/text", methods=['POST'])
+def text_reply():
+    with open('dis.json', 'r') as f:
+        intents = json.load(f)
+    if torch.cuda.is_available():
+        map_location = lambda storage, loc: storage.cuda()
+    else:
+        map_location = 'cpu'
+
+    FILE = "data.pth"
+    data = torch.load(FILE, map_location=map_location)
+    
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data["all_words"]
+    tags = data["tags"]
+    model_state = data["model_state"]
+    
+    model = NeuralNet(input_size, hidden_size, output_size)
+    model.load_state_dict(model_state)
+    model.eval()
+
+    while True:
+        # Fetch the message
+        msg = request.form["input"]
+        msg = tokenize(msg)
+        X = bag_of_words(msg, all_words)
+        X = X.reshape(1, X.shape[0])
+        X = torch.from_numpy(X)
+
+        output = model(X)
+        _, predicted = torch.max(output, dim = 1)
+        tag = tags[predicted.item()]
+    
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
+
+        if prob.item() > 0.75:
+            for intent in intents["intents"]:
+                if tag == intent["tag"]:
+                    # Create reply
+                    resp = random.choice(intent['responses']).format(msg)
+            
+        else:
+            resp = "I do not understand...".format(msg)
+    
+        return str(resp)
+
 if __name__ == "__main__":
     app.run(debug=False)
